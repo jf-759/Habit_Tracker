69381e9a2627872732718f8263670c20e3c3422a
jf-759 <jane.fernandez@students.dominican.edu>
2025-01-11 16:29:50 -0800

    project init

---
+++ tracker.py
@@ -0,0 +1,8 @@
+class Habit:
+    def __init__(self, name, frequency):
+        """
+        Initialize a habit with a name, frequency, and default values.
+        """
+
+        self.name = name
+        self.frequency = frequency
